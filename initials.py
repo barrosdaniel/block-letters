368fceb3eea55cc32fc6b54e290f102a8c856c18
Daniel Barros <daniel.barros@cqumail.com>
2023-10-22 12:09:54 +0800

    feature: Complete initials block letters

---
+++ initials.py
@@ -0,0 +1,10 @@
+# Daniel Barros
+# I am a Python Developer
+
+print('DDDD    BBBB ')
+print('D   D   B   B')
+print('D   D   B   B')
+print('D   D   BBBB ')
+print('D   D   B   B')
+print('D   D   B   B')
+print('DDDD    BBBB ')
